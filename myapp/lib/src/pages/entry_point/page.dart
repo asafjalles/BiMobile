e385014e0b7ef2fce1f188f59d3579a11cb766a0
Asaf Jalles <68341748+asafjalles@users.noreply.github.com>
2022-01-31 14:55:55 -0300

    feat(new version): Add new version

---
+++ myapp/lib/src/pages/entry_point/page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'package:myapp/auth/auth_state.dart';
+
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:myapp/src/pages/index.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:map/map.dart';
+import 'package:latlng/latlng.dart';
+import 'package:paged_vertical_calendar/paged_vertical_calendar.dart';
+
+class PageEntryPoint extends StatefulWidget {
+  const PageEntryPoint({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<PageEntryPoint> {
+  final datasets = <String, dynamic>{};
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF000000),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
+            width: double.maxFinite,
+            decoration: const BoxDecoration(
+              color: Color(0xFFFFFFFF),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 40,
+                top: 40,
+                right: 40,
+                bottom: 40,
+              ),
+              child: Text(r'''VACINOMETRO''',
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      color: const Color(0xFF000000),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 40,
+                      fontStyle: FontStyle.normal,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 1),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
